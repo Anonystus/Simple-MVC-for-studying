53dd77d2de129c11abd884a950a54f8128bd903f
Anonystus <33179522+Anonystus@users.noreply.github.com>
2017-11-02 01:46:11 +0500

    Update model.php

--- model.php
+++ model.php
@@ -1,5 +1,5 @@
 <?php
-function model($kek){//коннеткимся к ДБ с параметрами полученными через контроллер например ВЫБРАТЬ ИМЯ ИЗ ЮЗЕРОВ КОГДА ИД=ИД
+function model($kek){
 $keke="Your query was ";
 $kekes=" and it is ";
 $keks="!DYNAMIC ANSWER FROM DB FILTRED BY YOUR CONTROLLER PARAMETRES TO MODEL!";
@@ -7,6 +7,6 @@ $allkeks=$keke;
 $allkeks.= $kek;
 $allkeks.=$kekes;
 $allkeks.=$keks;
-return $allkeks; //1 это ответ  ответ ДБ с тем что нужно пользоваетлю через контроллер
+return $allkeks; 
 }
 ?>
